399aafc19bfd4c16e6709eb02c4f56b1cf145907
lingxuan <lingxuan016@cycu.org.tw>
2022-03-10 22:21:46 +0800

    first commit

---
+++ js/chunk-2d21e30b.e269dfe3.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d21e30b"],{d504:function(n,e,t){"use strict";t.r(e);var c=t("7a23");function d(n,e,t,d,r,u){return Object(c["t"])(),Object(c["e"])("div",null," σ ﾟ∀ ﾟ) ﾟ∀ﾟ)σ ")}var r={name:"Index",data:function(){return{}}},u=t("d959"),a=t.n(u);const o=a()(r,[["render",d]]);e["default"]=o}}]);
+//# sourceMappingURL=chunk-2d21e30b.e269dfe3.js.map
